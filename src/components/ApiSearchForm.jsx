b677df53090e9b4ccbe3b8f9fe93a4b9967f3e5d
pepperz <butlerbryan00@gmail.com>
2023-12-07 19:39:19 +1100

    env set up

--- src/components/ApiSearchForm.jsx
+++ src/components/ApiSearchForm.jsx
@@ -8,15 +8,15 @@ import { ApiContext } from "../contexts/ApiProvider";
 import { PokemonTeamContext } from "../contexts/PokemonDataProvider";
 
 export function ApiSearchForm(){
-	let apiUrlBase = useContext(ApiContext);
-    let pokemonDataArray =useContext(PokemonTeamContext);
+	let {url} = useContext(ApiContext);
+	let pokemonDataArray = useContext(PokemonTeamContext);
 	let [searchData, setSearchData] = useState("pikachu");
 
 	const searchForPokemon = async () => {
 		// console.log(`API URL is: ${apiUrlBase}`);
-		let response = await fetch(apiUrlBase + "pokemon/" + searchData);
-        let data = await response.json();
-        pokemonDataArray.push(data);
+		let response = await fetch(url + "pokemon/" + searchData);
+		let data = await response.json();
+		pokemonDataArray.push(data);
 	}
 
 	return(
